b6dbc307bcff31baf55a57aac3a3be78ba94db07
Wong Tom <23562997+tomwang1024@users.noreply.github.com>
2019-02-11 11:25:46 +0800

    used bcrypt for user's password hashing and store user's information securely. Added the ability to let the user to enter the password.

--- models/user.js
+++ models/user.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
-	username: {type: String, required: true}
+	username: {type: String, required: true},
+	password: {type: String, required: true}
+});
+
+// before we save the user data, call this function and hash the user's password
+userSchema.pre('save', function(next) {
+	var user = this;
+	bcrypt.hash(user.password, 10, function(err, hash) {
+		if (err) {
+			console.error(err);
+		}
+		user.password = hash;
+		next();
+	});
 });
 
 const User = mongoose.model('User', userSchema);
