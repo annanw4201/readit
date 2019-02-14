5f1125d4dd06dcd410710ff02faab2c4f345cea1
Wong Tom <23562997+tomwang1024@users.noreply.github.com>
2019-02-14 14:35:50 +0800

    Used authorization to enable if a user can view a secure page. Added a Room model.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var auth = require('./helpers/auth');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', auth.requireLogin, function(req, res, next) {
   User.find({}, 'username', function(err, users) {
   	if (err) {
   		console.error(err);
